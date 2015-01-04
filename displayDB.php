082f55dbbf9cadfa77a6fd739f7d0d40bd2b4f7a
oldpig <coy.zhu@hotmail.com>
2015-01-04 15:31:27 +0800

    add session to displayDB.php, page will return login.html without session id.

--- displayDB.php
+++ displayDB.php
@@ -1,4 +1,13 @@
 ﻿<?php
+
+session_start();
+
+//检测是否登录，若没登录则转向登录界面
+if(!isset($_SESSION['id'])){
+	header("Location:login.html");
+	exit();
+}
+
 $connection = mysql_connect("127.0.0.1", "george", "iamhgj");
 if (mysql_error())
   echo "sorry, there was an errno: ";
@@ -16,5 +25,6 @@ while($row = mysql_fetch_array($results)) {
     echo "<tr><td>$row[id] </td><td> $row[name]</td> <td> $row[email] </td></tr>";
 }
 echo "</table>";
+echo '<a href="login.php?action=logout">注销</a> 登录<br />';
 mysql_close($connection);
 ?>
